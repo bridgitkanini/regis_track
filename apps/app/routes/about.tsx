9d8ff56e2e31559c5e21e800b9706e0498993aff
bridgitkanini <bridgit.k.mutinda@gmail.com>
2025-09-18 00:13:20 +0300

    Create new about page

---
+++ apps/app/routes/about.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the about page for RegisTrack.</p>
+      <Link to="/">Back to home</Link>
+    </div>
+  );
+}
